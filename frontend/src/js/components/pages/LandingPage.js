0b5088e7ada4ff5d9a44f826a598715adb51e645
mateo-navarrete <mateonavarrete@pursuit.org>
2019-04-04 16:59:27 -0400

    added landingPage boilerplate

---
+++ frontend/src/js/components/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { DropRoutes, NavBar } from ".";
+
+export const LandingPage = props => {
+  return (
+    <>
+      <h1 className="lato">d r o p i n</h1>
+      <NavBar />
+      <DropRoutes />
+    </>
+  );
+};
--- frontend/src/js/components/pages/SwitchRouter.js
+++ frontend/src/js/components/pages/SwitchRouter.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
-import { EventsPage, SigninPage } from '.';
+import { EventsPage, LandingPage, SigninPage } from '.';
 
 export const SwitchRouter = props => {
   return (
     <Switch>
       <Route path="/events" render={withRouter(EventsPage)} />
       <Route path="/signin" render={withRouter(SigninPage)} />
-      <Route
-        path="/"
-        render={props => (
-          <div className="content">
-            <h1 className="lato">d r o p i n</h1>
-          </div>
-        )}
-      />
+      <Route path="/" render={withRouter(LandingPage)} />
     </Switch>
   );
 };
