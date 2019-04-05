9270781af06a7dbd7db02e0c24f671b13e2c44d3
jungraejang <jungraejang@pursuit.org>
2019-04-05 16:48:58 -0400

    Update PinsRouter.js

--- frontend/src/js/components/pins/PinsRouter.js
+++ frontend/src/js/components/pins/PinsRouter.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
-import { FoodPinsArea, MusicPinsArea } from '.';
+import { FoodPinsArea, MusicPinsArea, SportsPinsArea, ReligiousPinsArea, GamingPinsArea, EducationPinsArea, EntertainmentPinsArea, HumanitarianPinsArea, PartyPinsArea, FamilyPinsArea} from '.';
 
 export const PinsRouter = props => {
   return (
     <Switch>
       <Route path="/music" render={withRouter(MusicPinsArea)} />
       <Route path="/food" render={withRouter(FoodPinsArea)} />
+      <Route path="/sports" render={withRouter(SportsPinsArea)} />
+      <Route path="/religious" render={withRouter(ReligiousPinsArea)} />
+      <Route path="/gaming" render={withRouter(GamingPinsArea)} />
+      <Route path="/education" render={withRouter(EducationPinsArea)} />
+      <Route path="/entertainment" render={withRouter(EntertainmentPinsArea)} />
+      <Route path="/humanitarian" render={withRouter(HumanitarianPinsArea)} />
+      <Route path="/party" render={withRouter(PartyPinsArea)} />
+      <Route path="/family" render={withRouter(FamilyPinsArea)} />
     </Switch>
   );
 };
