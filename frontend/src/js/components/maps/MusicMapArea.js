a3eb7ed6b3d0c7beb577eb62bb20ac757028799f
mateo-navarrete <mateonavarrete@pursuit.org>
2019-04-05 14:15:18 -0400

    added music redux

--- frontend/src/js/components/maps/MusicMapArea.js
+++ frontend/src/js/components/maps/MusicMapArea.js
@@ -1,20 +1,18 @@
 import React from "react";
 import { withMusicDrops } from "../../containers";
 
-const authID = 1;
-
 const MusicMap = ({ drop, coords, ...props }) => {
-  console.log(drop, coords, props);
+  console.log("@musicMapArea", props);
   const renderMusicPins = coords.map((coord, i) => {
     return (
-      <>
+      <div key={"pin" + i}>
         <div>{drop + i}</div>
         <div>latitude: {coord.latitude}</div>
         <div>latitude: {coord.longitude}</div>
-      </>
+      </div>
     );
   });
-  return { renderMusicPins };
+  return <>{renderMusicPins}</>;
 };
 
-export const MusicMapArea = withMusicDrops(MusicMap, authID);
+export const MusicMapArea = withMusicDrops(MusicMap);
