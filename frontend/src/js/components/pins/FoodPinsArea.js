c65a4c32becea5049860f227cb11a734a06f1ce5
jungraejang <jungraejang@pursuit.org>
2019-04-05 16:48:49 -0400

    Update FoodPinsArea.js

--- frontend/src/js/components/pins/FoodPinsArea.js
+++ frontend/src/js/components/pins/FoodPinsArea.js
@@ -1,20 +1,9 @@
 import React from "react";
 import { withFoodDrops } from "../../containers";
-// import { Map } from "..";
+import { Map } from "..";
 
 const FoodPins = ({ drop, coords, ...props }) => {
-  console.log("@foodPinsArea", props);
-  const renderFoodPins = coords.map((coord, i) => {
-    return (
-      <div key={"pin" + i}>
-        <div>{drop + i}</div>
-        <div>latitude: {coord.latitude}</div>
-        <div>latitude: {coord.longitude}</div>
-      </div>
-    );
-  });
-  return <>{renderFoodPins}</>;
+  return <><Map coords={coords}/></>;
 };
-// <Map />
 
 export const FoodPinsArea = withFoodDrops(FoodPins);
