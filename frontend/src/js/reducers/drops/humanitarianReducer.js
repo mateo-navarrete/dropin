521c18576572680a05ff6c57fae4978741d2f8ca
mateo-navarrete <mateonavarrete@pursuit.org>
2019-04-05 12:26:21 -0400

    fixing redux

---
+++ frontend/src/js/reducers/drops/humanitarianReducer.js
@@ -0,0 +1,27 @@
+// import { AUTH_ERROR, AUTH_USER, AUTHORIZE_USER } from '../constants';
+
+// Latitude, Longitude
+// 40.743431, -73.938770
+// 40.742878, -73.941538
+// 40.743203, -73.942879
+// 40.743170, -73.943458
+// 40.743487, -73.939252
+// 40.742633, -73.940853
+// 40.743665, -73.940997
+// 40.744681, -73.939323
+
+const initState = {
+  drop: 'Humanitarian',
+  coords: [
+    { latitude: 40.743431, longitude: -73.93877 },
+    { latitude: 41.543431, longitude: -72.53877 },
+  ],
+};
+
+export const humanitarianReducer = (state = initState, action) => {
+  // let nextState;
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
