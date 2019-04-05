ac34ab867d317c19e22a6946eefc3ee75ebb4f33
AJ <atibanurse@ac.c4q.nyc>
2019-04-05 11:25:44 -0400

    added drops.js route file

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var users = require('./routes/users');
+var drops = require('./routes/drops');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', users);
+app.use('/drops', drops);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
