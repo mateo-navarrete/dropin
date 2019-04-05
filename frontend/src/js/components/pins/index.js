52549e07a183ea04a8d20221432700524f0e01c1
jungraejang <jungraejang@pursuit.org>
2019-04-05 16:48:53 -0400

    Update index.js

--- frontend/src/js/components/pins/index.js
+++ frontend/src/js/components/pins/index.js
@@ -1,6 +1,27 @@
-import { FoodPinsArea } from './FoodPinsArea';
-import { MusicPinsArea } from './MusicPinsArea';
-import { PinsRouter } from './PinsRouter';
-import { PinsArea } from './PinsArea';
+import { FoodPinsArea } from "./FoodPinsArea";
+import { MusicPinsArea } from "./MusicPinsArea";
+import { PinsRouter } from "./PinsRouter";
+import { PinsArea } from "./PinsArea";
+import { SportsPinsArea } from "./SportsPinsArea";
+import { ReligiousPinsArea } from "./ReligiousPinsArea";
+import { GamingPinsArea } from "./GamingPinsArea";
+import { EducationPinsArea } from "./EducationPinsArea";
+import { EntertainmentPinsArea } from "./EntertainmentPinsArea";
+import { HumanitarianPinsArea } from "./HumanitarianPinsArea";
+import { PartyPinsArea } from "./PartyPinsArea";
+import { FamilyPinsArea } from "./FamilyPinsArea";
 
-export { FoodPinsArea, MusicPinsArea, PinsRouter, PinsArea };
+export {
+  FoodPinsArea,
+  MusicPinsArea,
+  PinsRouter,
+  PinsArea,
+  SportsPinsArea,
+  ReligiousPinsArea,
+  GamingPinsArea,
+  EducationPinsArea,
+  EntertainmentPinsArea,
+  HumanitarianPinsArea,
+  PartyPinsArea,
+  FamilyPinsArea
+};
