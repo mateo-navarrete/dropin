cb924771e6faa02de1d6176ae368d5465b642351
jungraejang <jungraejang@pursuit.org>
2019-04-05 16:48:56 -0400

    Update MusicPinsArea.js

--- frontend/src/js/components/pins/MusicPinsArea.js
+++ frontend/src/js/components/pins/MusicPinsArea.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { withMusicDrops } from "../../containers";
-// import { Map } from "..";
+import { Map } from "..";
 
 const MusicPins = ({ drop, coords, ...props }) => {
-  console.log("@musicPinsArea", props);
-  const renderMusicPins = coords.map((coord, i) => {
-    return (
-      <div key={"pin" + i}>
-        <div>{drop + i}</div>
-        <div>latitude: {coord.latitude}</div>
-        <div>latitude: {coord.longitude}</div>
-      </div>
-    );
-  });
-  return <>{renderMusicPins}</>;
+  return (
+    <>
+    <Map coords={coords}/>
+    </>
+)
 };
 // <Map />
 
