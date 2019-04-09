1a1038125d7a9c7743f4ed89a55a9703df435f9f
mateo-navarrete <mateonavarrete@pursuit.org>
2019-04-09 10:09:48 -0400

    updated sizes

--- frontend/src/js/components/drops/DropsArea.js
+++ frontend/src/js/components/drops/DropsArea.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyledDropArea } from '..';
 
 export const DropsArea = ({ height, width, ...props }) => {
-  console.log(height, width);
+  // console.log(height, width);
   return (
     <div className="flex center" style={{ height: height, width: width }}>
       <div className="">
