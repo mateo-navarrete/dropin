a66cdba5e5166676054edbc9b7177f368ba3e439
mateo-navarrete <mateonavarrete@pursuit.org>
2019-04-09 17:32:27 -0400

    added stepper modal

--- frontend/src/js/actions/modals/stepperModalActions.js
+++ frontend/src/js/actions/modals/stepperModalActions.js
@@ -1,7 +1,7 @@
 import { OPEN_STEPPER_MODAL, CLOSE_STEPPER_MODAL } from '../../constants';
 
 export const openStepperModal = value => {
-  console.log('@osm', value);
+  // console.log('@osm', value);
   return { type: OPEN_STEPPER_MODAL, payload: value };
 };
 
