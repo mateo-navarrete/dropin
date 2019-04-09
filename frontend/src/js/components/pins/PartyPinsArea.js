d0656d409aa36cfd2875c2d1d4d6d52cbedd3883
jungraejang <jungraejang@pursuit.org>
2019-04-09 16:29:38 -0400

    Update PartyPinsArea.js

--- frontend/src/js/components/pins/PartyPinsArea.js
+++ frontend/src/js/components/pins/PartyPinsArea.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { withPartyDrops } from "../../containers";
 import { Map } from "..";
+import {PinModal} from "./modal/PinModal"
+
 
 const PartyPins = ({ drop, coords, ...props }) => {
   return (
     <>
     <Map coords={coords}/>
+    <PinModal coords={coords} />
     </>
 )
 };
