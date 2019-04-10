2bbb19e8746d0c43dedf5c13270dac98bd4ab828
AJ <atibanurse@ac.c4q.nyc>
2019-04-10 18:04:35 -0400

    backend auth functions complete and working properly.

--- backend/db/auth/passport.js
+++ backend/db/auth/passport.js
@@ -3,15 +3,17 @@ const passport = require("passport");
 const db = require('..');
 
 module.exports = () => {
-  passport.serializeUser((user, done) => {
+  passport.serializeUser((user, done) => { 
     done(null, user.user_name);
   });
 
   passport.deserializeUser((user_name, done) => {
     db.one("SELECT * FROM users WHERE user_name = ${user_name}", {
-      username: user_name
+      user_name: user_name
     })
       .then(user => {
+        console.log(user);
+        
         done(null, user.user_name);
       })
       .catch(err => {
