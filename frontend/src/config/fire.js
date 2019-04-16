b76cb81a57ada6d59ed89498585c6e13a22f84cb
jungraejang <jungraejang@pursuit.org>
2019-04-16 15:44:17 -0400

    Create fire.js

---
+++ frontend/src/config/fire.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyDL_jEstE_tT592DuEfyHwW1SgPOMCXwvM",
+  authDomain: "dropin-8f544.firebaseapp.com",
+  databaseURL: "https://dropin-8f544.firebaseio.com",
+  projectId: "dropin-8f544",
+  storageBucket: "dropin-8f544.appspot.com",
+  messagingSenderId: "262572318909"
+};
+
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+const fire = firebase.initializeApp(config);
+export {fire, googleProvider}
