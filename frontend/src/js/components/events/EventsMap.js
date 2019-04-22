f5633daf73600d19ca9e035f0829ffc522f3e17c
jungraejang <jungraejang@pursuit.org>
2019-04-22 17:42:24 -0400

    Update EventsMap.js

--- frontend/src/js/components/events/EventsMap.js
+++ frontend/src/js/components/events/EventsMap.js
@@ -7,7 +7,239 @@ import { css } from "@emotion/core";
 import { CircleLoader } from "react-spinners";
 import LoadingSpinner from "./LoadingSpinner";
 
-const exampleMapStyles = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]
+const exampleMapStyles = [
+  {
+    featureType: "administrative",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#6195a0"
+      }
+    ]
+  },
+  {
+    featureType: "administrative.province",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "landscape",
+    elementType: "geometry",
+    stylers: [
+      {
+        lightness: "0"
+      },
+      {
+        saturation: "0"
+      },
+      {
+        color: "#f5f5f2"
+      },
+      {
+        gamma: "1"
+      }
+    ]
+  },
+  {
+    featureType: "landscape.man_made",
+    elementType: "all",
+    stylers: [
+      {
+        lightness: "-3"
+      },
+      {
+        gamma: "1.00"
+      }
+    ]
+  },
+  {
+    featureType: "landscape.natural.terrain",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#bae5ce"
+      },
+      {
+        visibility: "on"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "all",
+    stylers: [
+      {
+        saturation: -100
+      },
+      {
+        lightness: 45
+      },
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#fac9a9"
+      },
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text",
+    stylers: [
+      {
+        color: "#4e4e4e"
+      }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#787878"
+      }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "transit",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  {
+    featureType: "transit.station.airport",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        hue: "#0a00ff"
+      },
+      {
+        saturation: "-77"
+      },
+      {
+        gamma: "0.57"
+      },
+      {
+        lightness: "0"
+      }
+    ]
+  },
+  {
+    featureType: "transit.station.rail",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#43321e"
+      }
+    ]
+  },
+  {
+    featureType: "transit.station.rail",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        hue: "#ff6c00"
+      },
+      {
+        lightness: "4"
+      },
+      {
+        gamma: "0.75"
+      },
+      {
+        saturation: "-68"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#eaf6f8"
+      },
+      {
+        visibility: "on"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#c7eced"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        lightness: "-49"
+      },
+      {
+        saturation: "-53"
+      },
+      {
+        gamma: "0.79"
+      }
+    ]
+  }
+];
 
 const EventMap = props => {
   const {
@@ -29,18 +261,18 @@ const EventMap = props => {
     <>
       {latitude && (
         <GoogleMap
-        options={{
-            styles: exampleMapStyles,
-        }}
+          options={{
+            styles: exampleMapStyles
+          }}
           defaultZoom={15}
           defaultCenter={{ lat: latitude, lng: longitude }}
           defaultClickableIcons={false}
           containerProps={{
-          ...props,
-          style: {
-            height: "100%",
-          },
-        }}
+            ...props,
+            style: {
+              height: "100%"
+            }
+          }}
         >
           {isMarkerShown && (
             <EventMarker
