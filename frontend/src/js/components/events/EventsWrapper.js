db8d7b53b6e2983781b939da3b4a3e76c5437257
jungraejang <jungraejang@pursuit.org>
2019-04-22 11:55:24 -0400

    Update EventsWrapper.js

--- frontend/src/js/components/events/EventsWrapper.js
+++ frontend/src/js/components/events/EventsWrapper.js
@@ -7,29 +7,36 @@ import {
   withSportsEvents,
   withUserCoords
 } from "../../containers";
+import LoadingSpinner from "./LoadingSpinner"
 
 const prepend = "https://maps.googleapis.com/maps/api/js?key=";
 const apiKey = "AIzaSyB5uKfMriNA73mQgW_ZRelAixBLEdqT-Xg";
 const append = "&v=3.exp&libraries=geometry,drawing,places";
 
 const EventWrapper = props => {
+  // let loaded = false
   let style = { height: props.mainHeight };
   const mapURL = prepend + apiKey + append;
   if (props.loaded && props.eventCoords.length !== 0) {
     return (
       <>
+      {!props.userCoords.latitude && (
+        <LoadingSpinner {...props} style={{ ...style, paddingTop: (props.mainHeight / 2) >> 0 }} />
+      )}
+      {props.userCoords.latitude && (
         <EventsMap
           isMarkerShown
           googleMapURL={mapURL}
-          loadingElement={
-            <div style={{ ...style, paddingTop: (props.mainHeight / 2) >> 0 }}>
-              Loading...
-            </div>
-          }
           containerElement={<div style={style} />}
           mapElement={<div style={style} />}
+          loadingElement={
+            <div style={{ ...style, paddingTop: (props.mainHeight / 2) >> 0 }}>
+            </div>
+          }
           {...props}
         />
+      )}
+
       </>
     );
   } else {
@@ -42,3 +49,9 @@ const EventsWrapper = withAreaSizes(withUserCoords(EventWrapper));
 export const FamilyEvents = withFamilyEvents(EventsWrapper);
 export const PartyEvents = withPartyEvents(EventsWrapper);
 export const SportsEvents = withSportsEvents(EventsWrapper);
+
+// loadingElement={
+//   <div style={{ ...style, paddingTop: (props.mainHeight / 2) >> 0 }}>
+//     Loading...
+//   </div>
+// }
