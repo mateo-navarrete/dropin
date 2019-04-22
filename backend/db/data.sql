b1ff5668d5a7938f1a0bffe94b54d4eefb1db532
mateo-navarrete <mateonavarrete@pursuit.org>
2019-04-22 12:30:29 -0400

    updating event queries & stepper

--- backend/db/data.sql
+++ backend/db/data.sql
@@ -61,13 +61,14 @@ INSERT INTO users (user_name, password_digest, email) VALUES ('JRJMuzik', '12345
 INSERT INTO events (category_id, user_id, latitude, longitude, display_user, event_name, description)
 VALUES ( 1, 1, 40.743431, -73.938770, 'true', 'My Family Event', 'Fun get together' ),
 ( 2, 1, 40.742431, -73.938770, 'true',  'My Party Event', 'So fun' ),
-( 3, 1, 40.742431, -73.938770, 'true',  'My Sports Event', 'team squad battle drillz' ),
+-- ( 3, 1, 40.742431, -73.938770, 'true',  'My Sports Event', 'team squad battle drillz' ),
 ( 1, 2, 40.742431, -73.938770, 'true',  'Family Fun', 'for everyone' ),
 ( 2, 2, 40.743431, -73.938770, 'true',  'party rockin', 'everyday we shufflin' ),
-( 3, 3, 40.743431, -73.938770, 'true',  'racquetball', 'come play' ),
+-- ( 3, 3, 40.743431, -73.938770, 'true',  'racquetball', 'come play' ),
 ( 1, 3, 40.743431, -73.938770, 'true',  'family bbq', 'byob' ),
-( 2, 3, 40.741431, -73.938770, 'true',  'party everyday', 'rock n roll all night' ),
-( 3, 3, 40.741431, -73.938870, 'true',  'smash tourney 2000', 'big money no hanzo mains' );
+( 2, 3, 40.741431, -73.938770, 'true',  'party everyday', 'rock n roll all night' )
+-- ( 3, 3, 40.741431, -73.938870, 'true',  'smash tourney 2000', 'big money no hanzo mains' )
+;
 
 UPDATE events SET expiration_date = created_date + INTERVAL '15' MINUTE;
 
