283169ba171b3f044eba5a5c622d3c2919aaedb3
jungraejang <jungraejang@pursuit.org>
2019-05-02 12:39:35 -0400

    Update eventsRoutes.js

--- backend/routes/eventsRoutes.js
+++ backend/routes/eventsRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { createEvent, getEvents, updateEvent, deleteEvent } = require('../db/queries/eventsQueries');
 
 router.post('/', createEvent);
-router.get('/:id', getEvents);
+router.get('/:id?lat?lon', getEvents);
 router.put('/', updateEvent);
 router.delete('/:id', deleteEvent);
 
