b32db0b700cf5abb57493b65f8ff0f11fe866108
jungraejang <jungraejang@pursuit.org>
2019-05-08 15:30:30 -0400

    Update eventsReducer.js

--- frontend/src/js/reducers/events/eventsReducer.js
+++ frontend/src/js/reducers/events/eventsReducer.js
@@ -1,32 +1,37 @@
 import {
   GETTING_EVENTS,
   GOT_EVENTS_ERROR,
-  GOT_EVENTS_SUCCESS
-} from '../../constants';
+  GOT_EVENTS_SUCCESS,
+  GET_ADDRESS
+} from "../../constants";
 
 const initState = {
   eventsList: [],
-  errMsg: '',
+  errMsg: "",
   loading: false,
+  address: ""
 };
 
 export const eventsReducer = (state = initState, action) => {
   let nextState;
   switch (action.type) {
     case GETTING_EVENTS:
-      nextState = { ...state, errMsg: '', loading: true };
+      nextState = { ...state, errMsg: "", loading: true };
       return nextState;
     case GOT_EVENTS_ERROR:
-      console.log('@GotEventsErr_Reducer');
+      console.log("@GotEventsErr_Reducer");
       nextState = { ...state, errMsg: action.payload, loading: false };
       return nextState;
     case GOT_EVENTS_SUCCESS:
       nextState = {
         ...state,
         eventsList: action.payload,
-        errMsg: '',
-        loading: false,
+        errMsg: "",
+        loading: false
       };
+      return nextState;
+    case GET_ADDRESS:
+      nextState = { ...state, address: action.payload };
       return nextState;
     default:
       return state;
