84d679c1728d561549a80b56dd33eaf1dde60931
mateo-navarrete <mateonavarrete@pursuit.org>
2019-05-08 12:32:47 -0400

    updating

---
+++ backend/app.js
@@ -0,0 +1,59 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('./db/auth/local');
+const sendAPI = require('./routes/send');
+
+const indexRouter = require('./routes/index');
+const { eventsApi, usersApi } = require('./routes/api');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser('never gonna give u up'));
+
+app.use(
+  session({
+    secret: 'never gonna give u up',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api/events', eventsApi);
+app.use('/api/users', usersApi);
+app.use('/api/send', sendAPI);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
