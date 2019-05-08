21940330a15e11c68bd2518182cabacd5fc8ae1a
jungraejang <jungraejang@pursuit.org>
2019-05-07 20:04:56 -0400

    Update View.js

--- frontend/src/js/components/EventsActive/ActiveEvents/View.js
+++ frontend/src/js/components/EventsActive/ActiveEvents/View.js
@@ -4,11 +4,16 @@ import { ActivePinIcon } from '../../material';
 import { ChildListItem } from '../../utils';
 
 export const View = ({ getEvents, handleClose, ...props }) => {
+  let eventObj = {
+    url: "",
+    latitude: props.coords.latitude,
+    longitude: props.coords.longitude
+  }
   return (
     <ChildListItem
       primaryText="Active"
       handleClick={() => {
-        getEvents();
+        getEvents(eventObj);
         handleClose();
       }}
     >
