1b2592c6a826b7a04ed002878c6696f950a66fa0
mateo-navarrete <mateonavarrete@pursuit.org>
2019-05-08 15:27:01 -0400

    updated EventStepper forms

--- frontend/src/js/components/EventDraft/EventStepper/index.js
+++ frontend/src/js/components/EventDraft/EventStepper/index.js
@@ -2,25 +2,12 @@ import React, { Component } from 'react';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import { EventStepperControls } from '../EventStepperControls';
 import { EventNameForm } from '../forms';
 import { EventCaptionForm } from '../forms';
 
 const getSteps = () => ['', '', ''];
-
-// function getStepContent(stepIndex) {
-//   switch (stepIndex) {
-//     case 0:
-//       return 'at zero';
-//     case 1:
-//       return 'What is an ad group anyways?';
-//     case 2:
-//       return 'This is the bit I really care about!';
-//     default:
-//       return 'Unknown stepIndex';
-//   }
-// }
 
 class WrappedComponent extends Component {
   state = {
@@ -78,7 +65,6 @@ class WrappedComponent extends Component {
       ) : (
         ''
       );
-    // {getStepContent(activeStep)}
     return (
       <div className={classes.root}>
         <Stepper activeStep={activeStep} alternativeLabel>
@@ -89,12 +75,10 @@ class WrappedComponent extends Component {
           ))}
         </Stepper>
         <div>
-          <Typography className={classes.instructions}>
-            {renderEventNameForm}
-            {renderEventCaptionForm}
-          </Typography>
+          {renderEventNameForm}
+          {renderEventCaptionForm}
           <EventStepperControls
-            activeStep={activeStep}
+            {...this.state}
             classes={classes}
             handleBack={this.handleBack}
             handleNext={this.handleNext}
@@ -107,32 +91,9 @@ class WrappedComponent extends Component {
   }
 }
 
-export const EventStepper = WrappedComponent;
+// <Typography className={classes.instructions}>
+//   {renderEventNameForm}
+//   {renderEventCaptionForm}
+// </Typography>
 
-// <div>
-//   <Button
-//     disabled={activeStep === 0}
-//     onClick={this.handleBack}
-//     className={classes.backButton}
-//   >
-//     Back
-//   </Button>
-//   <Button
-//     disabled={activeStep === 0}
-//     onClick={this.handleReset}
-//     className={classes.backButton}
-//   >
-//     Reset
-//   </Button>
-//   <Button
-//     variant="contained"
-//     color="primary"
-//     onClick={
-//       activeStep === steps.length - 1
-//         ? this.handleReset
-//         : this.handleNext
-//     }
-//   >
-//     {activeStep === steps.length - 1 ? 'Drop Pin' : 'Next'}
-//   </Button>
-// </div>
+export const EventStepper = WrappedComponent;
--- frontend/src/js/components/EventDraft/EventStepperControls/index.js
+++ frontend/src/js/components/EventDraft/EventStepperControls/index.js
@@ -3,6 +3,8 @@ import Button from '@material-ui/core/Button';
 
 export const EventStepperControls = ({
   activeStep,
+  event_name,
+  caption,
   classes,
   handleBack,
   handleNext,
@@ -27,6 +29,9 @@ export const EventStepperControls = ({
         Reset
       </Button>
       <Button
+        disabled={
+          (activeStep === 0 && !event_name) || (activeStep === 1 && !caption)
+        }
         variant="contained"
         color="primary"
         onClick={activeStep === steps.length - 1 ? handleReset : handleNext}
