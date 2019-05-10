948c5108924fcb9524d6e37c1a24714e3cf03fc8
jungraejang <jungraejang@pursuit.org>
2019-05-10 15:14:10 -0400

    Update index.js

--- frontend/src/js/components/Map/UserMarker/index.js
+++ frontend/src/js/components/Map/UserMarker/index.js
@@ -1,13 +1,42 @@
 //jscs:disable requireShorthandArrowFunctions
-import React, { Fragment as F } from 'react';
-import { EventMarker } from '../../EventMarker';
+import React, { Fragment as F } from "react";
+import { EventMarker } from "../../EventMarker";
+import userMarkerIcon from "../../../../assets/user_marker.png";
+import { Marker } from "react-google-maps";
+
+// export const UserMarker = ({ isMarkerShown, ...props }) => {
+//   const renderView =
+//     isMarkerShown && props.position.lat ? (
+//       <EventMarker {...props} isUserMarker />
+//     ) : (
+//       ''
+//     );
+//   return <F>{renderView}</F>;
+// };
 
 export const UserMarker = ({ isMarkerShown, ...props }) => {
+  console.log("user marker", props);
+  const { lat, lng } = props.position;
+  // const image = {
+  //   url: userMarkerIcon,
+  //   scaledSize: new google.maps.Size(60, 60)
+  //   size: (60, 60)
+  // };
   const renderView =
     isMarkerShown && props.position.lat ? (
-      <EventMarker {...props} isUserMarker />
+      <Marker
+        onClick={props.handleClick}
+        position={{ lat: lat, lng: lng }}
+        key="UserMarker"
+        zIndex={100}
+        icon={{
+          url: userMarkerIcon,
+          scaledSize: new window.google.maps.Size(60, 60)
+        }}
+        id="UserMarker"
+      />
     ) : (
-      ''
+      ""
     );
   return <F>{renderView}</F>;
 };
