ccb0041c8ea8109dfafafb0e3a03b29a521129fa
jungraejang <jungraejang@pursuit.org>
2019-05-10 15:13:57 -0400

    Update index.js

--- frontend/src/js/components/Map/EventsMarkers/index.js
+++ frontend/src/js/components/Map/EventsMarkers/index.js
@@ -2,6 +2,7 @@
 import React, { Fragment as F } from 'react';
 // import { View } from './View';
 import { EventMarker } from '../../EventMarker';
+import Spiderfy from '../Spiderfy/Spiderfy.js'
 
 export const EventsMarkers = props => {
   let renderList =
@@ -16,9 +17,20 @@ export const EventsMarkers = props => {
   console.log("Even Markers Render List", renderList )
   const renderEvents = renderList
     ? props[renderList].map(e => {
+      console.log("Even Markers Map", e )
         let position = { lat: e.latitude, lng: e.longitude };
-        return <EventMarker key={'event-' + e.id} {...e} position={position} />;
+        return (
+        <EventMarker key={'event-' + e.id} {...e} position={position} />
+)
       })
     : '';
-  return <F>{renderEvents}</F>;
+  return (
+    <F>
+    {renderEvents ? (
+      <Spiderfy>
+      {renderEvents}
+      </Spiderfy>
+    ) : ("")}
+    </F>
+);
 };
