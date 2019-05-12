ac9b3af6b6cbb5e180ccc9d2e025bb85f9623826
jungraejang <jungraejang@pursuit.org>
2019-05-12 17:07:11 -0400

    Update index.js

--- frontend/src/js/components/Map/EventsMarkers/index.js
+++ frontend/src/js/components/Map/EventsMarkers/index.js
@@ -1,36 +1,35 @@
 //jscs:disable requireShorthandArrowFunctions
-import React, { Fragment as F } from 'react';
+import React, { Fragment as F } from "react";
 // import { View } from './View';
-import { EventMarker } from '../../EventMarker';
-import Spiderfy from '../Spiderfy/Spiderfy.js'
+import { EventMarker } from "../../EventMarker";
+import Spiderfy from "../Spiderfy/Spiderfy.js";
 
 export const EventsMarkers = props => {
   let renderList =
     props.eventsList && props.eventsList.length
-      ? 'eventsList'
+      ? "eventsList"
       : props.userEventsList && props.userEventsList.length
-      ? 'userEventsList' //switch to userEventsList later
-      : props.userHistory && props.userHistory.length
-      ? 'userHistory'
-      : null;
+        ? "userEventsList" //switch to userEventsList later
+        : props.userHistory && props.userHistory.length
+          ? "userHistory"
+          : null;
   // console.log(renderList, props);
-  console.log("Even Markers Render List", renderList )
-  const renderEvents = renderList
+  console.log("Event Markers Render List", renderList);
+  let renderEvents = renderList
     ? props[renderList].map(e => {
-      console.log("Even Markers Map", e )
+        console.log("Even Markers Map", e);
         let position = { lat: e.latitude, lng: e.longitude };
         return (
-        <EventMarker key={'event-' + e.id} {...e} position={position} />
-)
+          <F>
+          <EventMarker key={"event-" + e.id} {...e} position={position} />
+          </F>
+        )
       })
-    : '';
+    : "";
+    console.log("render Events", renderEvents)
   return (
-    <F>
-    {renderEvents ? (
-      <Spiderfy>
-      {renderEvents}
-      </Spiderfy>
-    ) : ("")}
-    </F>
-);
+    <div>
+    {renderEvents}
+    </div>
+  )
 };
