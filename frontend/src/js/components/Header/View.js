7581ecf27a89b10f0d542dfdeca3c68b120e4bf4
jungraejang <jungraejang@pursuit.org>
2019-05-13 12:50:55 -0400

    Update View.js

--- frontend/src/js/components/Header/View.js
+++ frontend/src/js/components/Header/View.js
@@ -1,7 +1,7 @@
 //jscs:disable requireShorthandArrowFunctions
 import React from 'react';
 import { Wrapper } from './Wrapper';
-// import { EventsMenu } from '../EventsMenu';
+import { EventsMenu } from '../EventsMenu';
 import { HeaderLogo } from '../logo';
 import { AuthMenu } from '../AuthMenu';
 
@@ -9,11 +9,13 @@ import { AuthMenu } from '../AuthMenu';
 export const View = ({ ...props }) => {
   return (
     <Wrapper {...props}>
+    <EventsMenu />
       <HeaderLogo
         height={props.height}
         isCompact={props.isCompact}
         color="red"
       />
+
       <AuthMenu />
     </Wrapper>
   );
