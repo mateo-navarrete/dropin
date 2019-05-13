1fd4f4289f1020801e1a3590f2c307ed05ab3694
jungraejang <jungraejang@pursuit.org>
2019-05-12 22:45:09 -0400

    Update index.js

--- frontend/src/js/components/Map/UserMarker/index.js
+++ frontend/src/js/components/Map/UserMarker/index.js
@@ -35,9 +35,8 @@ export const UserMarker = ({ isMarkerShown, ...props }) => {
         }}
         id="UserMarker"
       />
-
     ) : (
       ""
     );
-  return <F>{renderView}</F>;
+  return <F>{renderView}</F>
 };
